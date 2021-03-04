1b8998f3201726d555a66a5ab8812c8b2a47ad0d
Florindo <flolopdel@gmail.com>
2021-03-04 23:22:59 +0100

    add palencia sample

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -12,32 +12,60 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+
+      <ion-card>
+        <ion-item>
+          <ion-icon icon="pin" slot="start"></ion-icon>
+          <ion-label>{{info.Ciudad}}</ion-label>
+          <ion-button fill="outline" slot="end">{{info.Comunidad}}</ion-button>
+        </ion-item>
+
+        <ion-card-content>
+          Bares
+          <ion-list>
+            <ion-item :key="key" v-for="(bar,key) in info.Bares">
+              <ion-label>{{bar.Nombre}}</ion-label>
+            </ion-item>
+          </ion-list>
+          Ocio
+          <ion-list>
+            <ion-item :key="key" v-for="(bar,key) in info.Ocio">
+              <ion-label>{{bar.Nombre}}</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script >
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import axios from "axios";
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   data(){
     return {
-      jsonObject: {}
-    };
+      info: null,
+      loading: true,
+      errored: false
+    }
   },
-  created() {
-    this.init();
+  mounted  () {
+      axios
+      .get('https://raw.githubusercontent.com/Arrako23/LDM/master/PR%C3%81CTICAS/P-2-LDM-2/ocio.json')
+      .then(response => {
+        this.info = response.data;
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
   },
   methods: {
-    init() {
-      fetch("https://raw.githubusercontent.com/Arrako23/LDM/master/PR%C3%81CTICAS/P-2-LDM-2/ocio.json")
-      .then(response => response.json())
-      .then(data => (this.jsonObject = data));
-    }
   }
 }
 </script>
